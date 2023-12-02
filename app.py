c93cfcde7959ca973c53e533e5bf0b718700304a
vamshiamudala <vamshidhar127@gmail.com>
2023-12-01 18:12:15 -0600

    final commit

--- app.py
+++ app.py
@@ -4,27 +4,33 @@ import json
 import stripe
 from twilio.rest import Client
 
+
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Needed for session managementx``
+
+
 # Set your Twilio credentials
 twilio_account_sid = 'ACe990909ae89799958980653f744f3b2d'
-twilio_auth_token = '4d7311b7d41cd118b8d645771ce1331e'
+twilio_auth_token = '53fbd43721f3f0898abb7d2983f3b22d'
 twilio_phone_number = 'whatsapp:+14155238886'
 twilio_client = Client(twilio_account_sid, twilio_auth_token)
 
 # Set your test secret key for Stripe
 stripe.api_key = "sk_test_51OIS6hGfhJguOr7SCrwqtCPyCvnBCwwEcHsbv79JtwAJunR2Su6ji4GZx5jrGlULmvT0Ipx5g3pJjqI4uc3kDMfU00uPCuGOtm"
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Needed for session management
+# Dummy initial balance (you should fetch this from your database)
+initial_balance = 1000
 
 # Hardcoded credentials
 USERNAME = "vamshiroyal"
 PASSWORD = "Royal@1999"
 
 
+
 @app.route("/")
 def start():
     return render_template('login.html')
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -32,18 +38,20 @@ def login():
     password = request.form['password']
 
     if username == USERNAME and password == PASSWORD:
+        # items = get_product_items()
+        # return render_template('mfa.html', items=items)
         # Store the username in the session
-        session['username'] = username
+            session['username'] = username
 
-        # Generate and send OTP via Twilio WhatsApp
-        otp_generated = generate_and_send_otp(username)
+            # Generate and send OTP via Twilio WhatsApp
+            otp_generated = generate_and_send_otp(username)
 
-        if otp_generated:
-            # Redirect to the MFA page
-            return redirect(url_for('otp_verification'))
-        else:
-            flash("OTP could not be sent. Please try again.")
-            return redirect(url_for('start'))
+            if otp_generated:
+                # Redirect to the MFA page
+                return redirect(url_for('otp_verification'))
+            else:
+                flash("OTP could not be sent. Please try again.")
+                return redirect(url_for('start'))
 
     else:
         flash("Login Failed")
@@ -80,6 +88,9 @@ def generate_and_send_otp(username):
         return False
 
 
+
+  
+    
 @app.route('/otp_verification', methods=['GET', 'POST'])
 def otp_verification():
     if 'username' not in session:
@@ -118,11 +129,6 @@ def resend_otp():
     return redirect(url_for('otp_verification'))  # Redirect back to OTP verification page
 
 
-@app.route('/home')
-def home():
-    items = get_product_items()  # This function call populates the items list
-    return render_template('home.html', items=items)
-
 
 def get_product_items():
     products_dir = os.path.join('static', 'items')
@@ -152,8 +158,15 @@ def get_product_items():
                 'information': information,
                 'price': price
             })
-
+            
     return products
+ 
+
+@app.route('/home')
+def home():
+    items = get_product_items()  # This function call populates the items list
+    return render_template('home.html', items=items)
+
 
 
 @app.route('/add_to_cart', methods=['POST'])
@@ -172,15 +185,13 @@ def add_to_cart():
     # Redirect to the cart page or back to the products page
     return redirect(url_for('show_cart'))  # or return redirect(url_for('home'))
 
-
 @app.route('/cart')
 def show_cart():
     # Display the cart items
     cart_items = session.get('cart', [])
     return render_template('cart.html', cart=cart_items)
 
-
-@app.route('/checkout', methods=['POST'])
+@app.route('/checkout')
 def checkout():
     if 'cart' not in session or not session['cart']:
         # If there is no cart or it's empty, redirect to the home or cart page
@@ -191,40 +202,65 @@ def checkout():
     total_price = sum(item.get('quantity', 1) * float(item['price'].replace('$', '')) for item in session['cart'])
 
     # Render the checkout page with the cart items and totals
-    return render_template('billing.html', cart=session['cart'], total_items=total_items, total_price=f"${total_price:.2f}")
+    return render_template('checkout.html', cart=session['cart'], total_items=total_items, total_price=f"${total_price:.2f}")
 
-
-# Define the route for processing the payment
-@app.route('/process_payment', methods=['POST'])
-def process_payment():
+@app.route('/process_checkout', methods=['GET', 'POST'])
+def process_checkout():
     if request.method == 'POST':
-        # Calculate the total amount of the payment
-        cart_items = session.get('cart', [])
-        total_price = sum(item.get('quantity', 1) * float(item['price'].replace('$', '')) for item in cart_items)
+        # Extract form data
+        shipping_info = {
+            'name': request.form.get('name'),
+            'email': request.form.get('email'),
+            'address': request.form.get('address'),
+            'city': request.form.get('city'),
+            'state': request.form.get('state'),
+            'zip_code': request.form.get('zip'),
+            'country': request.form.get('country'),
+        }
+        
+        
+        # Here, instead of saving to session, you might want to save to a database
+        session['shipping_info'] = shipping_info
+        
+        # Instead of redirecting to the order confirmation, redirect to payment function
+        return redirect(url_for('create_payment'))
 
-        try:
-            # Create a PaymentIntent
-            payment_intent = stripe.PaymentIntent.create(
-                amount=int(total_price * 100),  # Amount in cents
-                currency='usd',
-                payment_method_types=['card'],
-                description='Payment for products',
-                confirm=True,  # Confirm the PaymentIntent immediately
-            )
+    # If method is GET, or if any other method is used, redirect to home page as fallback
+    return redirect(url_for('home'))
 
-            # Return the client secret to the frontend
-            client_secret = payment_intent.client_secret
 
-            # Redirect to a payment success page or return the client secret to the frontend
-            return jsonify({'clientSecret': client_secret})
 
-        except stripe.error.StripeError as e:
-            # Handle any Stripe errors and return an error response
-            return jsonify({'error': str(e)})
 
-    # If the request method is not POST, return an error response
-    return jsonify({'error': 'Invalid request method'})
 
+@app.route('/create_payment')
+def create_payment():
+        # Calculate total items and total price
+    total_items = sum(item.get('quantity', 1) for item in session['cart'])  # Default quantity to 1 if not present
+    total_price = sum(item.get('quantity', 1) * float(item['price'].replace('$', '')) for item in session['cart'])
+    return render_template('test.html', total_items=total_items, total_price=f"${total_price:.2f}")
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    try:
+        amount = float(request.form['amount'])
+        token = request.form['token']  # Retrieve the token from the form
+
+        # Charge the customer using the token
+        charge = stripe.Charge.create(
+            amount=int(float(amount * 100)),  # Amount should be in cents
+            currency='usd',
+            source=token,
+        )
+
+        # Update the Stripe balance (this is just a simple example)
+        global initial_balance
+        initial_balance += amount
+
+        # return jsonify({"message": "Payment successful", "balance": initial_balance})
+        return redirect(url_for('payment_completed'))
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 @app.route('/payment_completed')
 def payment_completed():
@@ -235,12 +271,10 @@ def payment_completed():
     # Redirect to order confirmation page...
     return redirect(url_for('order_confirmation'))
 
-
 @app.route('/payment_canceled')
 def payment_canceled():
     # Handle the canceled payment...
     return redirect(url_for('cart'))
-
 
 @app.route('/order_confirmation')
 def order_confirmation():
@@ -253,5 +287,26 @@ def order_confirmation():
     return render_template('order_confirmation.html')
 
 
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+# import os
+# import stripe
+# from flask import Flask, render_template, request, jsonify
+
+# app = Flask(__name__)
+
+# # Set your Stripe API key
+# stripe.api_key = "sk_test_51OIS6hGfhJguOr7SCrwqtCPyCvnBCwwEcHsbv79JtwAJunR2Su6ji4GZx5jrGlULmvT0Ipx5g3pJjqI4uc3kDMfU00uPCuGOtm"
+
+# # Dummy initial balance (you should fetch this from your database)
+# initial_balance = 1000
+
+# @app.route('/test')
+# def test():
+#     return render_template('test.html')
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
